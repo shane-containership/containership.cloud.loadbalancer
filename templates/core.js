a72909e8b03fb327b03e216acd6727bdc1beb4be
Norman Joyner <norman.joyner@gmail.com>
2016-09-28 14:46:08 -0400

    Add support for https loadbalancers

--- templates/core.js
+++ templates/core.js
@@ -7,6 +7,7 @@ module.exports = {
     defaults: {
         err_log_level: 'warn',
         http_log_format: '$host $remote_addr [$time_local] "$request" $status $http_referer "$http_user_agent"',
+        server_names_hash_bucket_size: 1024,
         worker_connections: 512,
         worker_processes: 4
     },
@@ -25,10 +26,12 @@ events {
 error_log stderr ${options.err_log_level};
 
 http {
+    server_names_hash_bucket_size ${options.server_names_hash_bucket_size};
     server_tokens off;
     log_format logformat '${options.http_log_format}';
     access_log /proc/1/fd/1 logformat;
     include /etc/nginx/http.d/*.conf;
+    include /etc/nginx/https.d/*.conf;
 }
 
 
